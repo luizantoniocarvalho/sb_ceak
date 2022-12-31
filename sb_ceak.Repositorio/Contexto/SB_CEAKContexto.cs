a276c6b01135d14b35a42ab733f45b3ff673184d
luizantoniocarvalho <luiac13@hotmail.com>
2022-12-31 00:04:36 -0300

    Foi feito o arquivo DbContext e DbSet.

---
+++ sb_ceak.Repositorio/Contexto/SB_CEAKContexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sb_ceak.Dominio.Entidade;
+
+namespace sb_ceak.Repositorio.Contexto
+{
+    public class SB_CEAKContexto : DbContext
+    {
+        public DbSet<Autores> Autor                                 { get; set; }
+        public DbSet<EmprestimoDevolucoes> EmprestimoDevolucao      { get; set; }
+        public DbSet<Funcoes> Funcao                                { get; set; }
+        public DbSet<Logs> Log                                      { get; set; }
+        public DbSet<Obras> Obra                                    { get; set; }
+        public DbSet<Operacoes> Operacao                            { get; set; }
+        public DbSet<Parametros> Parametro                          { get; set; }
+        public DbSet<Perfis> Perfil                                 { get; set; }
+        public DbSet<Pessoas> Pessoa                                { get; set; }
+        public DbSet<Reservas> Reserva                              { get; set; }
+        public DbSet<Telefones> Telefone                            { get; set; }
+
+    }
+}
